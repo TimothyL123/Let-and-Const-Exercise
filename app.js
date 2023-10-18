60c2a808b3f578504ef727c94ab89ecc902626aa
Timothy Lewis <141955866+TimothyL123@users.noreply.github.com>
2023-10-18 10:41:01 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const PI = 3.141592653589793;
+if (true) {
+    const PI = 42;
+    console.log(PI);
+}
+console.log(PI);
+
+
+// 1. The difference between "var" and "let" is that "var" can reassign, redeclare, and mutate. However, "let" cannot redeclare.
+
+// 2. The difference between "var" and "const" is that "var" can reassign, redeclare, and mutate. However, "const" cannot reassign or redeclare.
+
+// 3. The difference between "let" and "const" is that "let" can mutate and reassign but not redeclare, but "const" can only mutate and not reassign nor redeclare.
+
+// 4. There variables are used before they are declared.
